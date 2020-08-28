f4114037be7edfce29308685f6eda7eac4c80966
sillybread <quoctoan.c2b@gmail.com>
2020-08-28 19:19:02 +0700

    repost build version

---
+++ 3dchart/build/precache-manifest.6ae7daeda1c6c80fa1055d29c7458225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559d6a19fe0419e7990aca7eaa1165df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606ef97fd8c7f33e33dd",
+    "url": "/static/css/main.54b63ebd.chunk.css"
+  },
+  {
+    "revision": "81eb9071d9cefae9edd9",
+    "url": "/static/js/2.f4e22ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4e22ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606ef97fd8c7f33e33dd",
+    "url": "/static/js/main.df3f8e56.chunk.js"
+  },
+  {
+    "revision": "67ecfe99ed7c8f44ef53",
+    "url": "/static/js/runtime-main.85e77acf.js"
+  }
+]);
