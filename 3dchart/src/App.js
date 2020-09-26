09becc2a935902b68b38176fb9e121b9198b7013
sillybread <quoctoan.c2b@gmail.com>
2020-09-26 18:12:18 +0700

    add fetch data function

--- 3dchart/src/App.js
+++ 3dchart/src/App.js
@@ -5,7 +5,7 @@ import SChart3DLite from './components/SChartLite.js'
 export default function App() {
   return (
     <div>
-      <SChart3DLite ref={self=>{self.setState({bg_color: 0xffffff})}}/>
+      <SChart3DLite ref={self=>{self.setState({bg_color: 0xaaffaa})}}/>
     </div>
   );
 }
--- 3dchart/src/components/SChartLite.js
+++ 3dchart/src/components/SChartLite.js
@@ -4,6 +4,9 @@ import React, {
 import * as THREE from 'three';
 import OrbitControls from 'orbit-controls-es6';
 import './SChartLite.css';
+import {
+    Object3D
+} from 'three';
 //import Axios from 'axios';
 
 export default class SChart3DLite extends Component {
@@ -41,8 +44,8 @@ export default class SChart3DLite extends Component {
         controls.enableDamping = true;
         controls.dampingFactor = 0.05;
         controls.screenSpacePanning = false;
-        controls.minDistance = 30;
-        controls.maxDistance = 50;
+        controls.minDistance = 10;
+        controls.maxDistance = 100;
         controls.maxPolarAngle = Math.PI;
 
         var axesHelper = new THREE.AxesHelper(7749);
@@ -54,7 +57,7 @@ export default class SChart3DLite extends Component {
             renderer.setSize(window.innerWidth, window.innerHeight);
         }, false);
 
-        this.doStuff(scene);
+        this.buildChart(scene);
 
         var animate = function () {
             requestAnimationFrame(animate);
@@ -64,126 +67,196 @@ export default class SChart3DLite extends Component {
         animate();
     }
 
-    doStuff(oScene){
+    buildChart(oScene) {
+        //let aData = [[[ 63,  40,  160], [ 120,  98,  113], [ 153,  150,  135]], [[ -41,  66,  -14], [ 141,  121,  76], [ -44,  150,  160]], [[ 130,  92,  46], [ -28,  103,  45], [ 102,  78,  78]], [[ 130,  92,  46], [ -28,  103,  45], [ 102,  78,  78]]];
+        let aData = [
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [9, 10, 11]
+            ],
+            [
+                [12, 13, 14],
+                [15, 16, 17],
+                [18, 19, 20],
+                [21, 22, 23]
+            ],
+            [
+                [24, 25, 26],
+                [27, 28, 29],
+                [30, 31, 32],
+                [33, 34, 35]
+            ],
+            [
+                [36, 37, 38],
+                [39, 40, 41],
+                [42, 43, 44],
+                [45, 46, 47]
+            ],
+            [
+                [48, 49, 50],
+                [51, 52, 53],
+                [54, 55, 56],
+                [57, 58, 59]
+            ]
+        ];
         let size = {
-            x: 40, 
-            y: 40, 
-            z: 40,
-            get X(){
-                return this.x*this.tileSize;
+            x: aData[0][0].length,
+            y: aData[0].length,
+            z: aData.length,
+            get X() {
+                return this.x * this.tileSize;
             },
-            get Y(){
-                return this.y*this.tileSize;
+            get Y() {
+                return this.y * this.tileSize;
             },
-            get Z(){
-                return this.z*this.tileSize;
+            get Z() {
+                return this.z * this.tileSize;
             },
             tileSize: 1
         };
 
-        let cube = new Array(6);
-        for (let ii=0;ii<6;ii++){
-            cube[ii] = createAFace(size, ii);
-            setColor(cube[ii]);
-            oScene.add(cube[ii]);
+        let cube = new Object3D();
+        for (let ii = 0; ii < 6; ii++) {
+            cube.add(createAFace(size, ii));
         }
-        
-        function setColor(face){
-            console.log(tempToHSL(0,10,20));
-            let color = 0;
-            let n = face.geometry.faces.length/2;
-            for (let ii=0;ii<n;ii++){
-                color = tempToHSL(0, n, ii);
-                face.geometry.faces[ii*2].color.set(color);
-                face.geometry.faces[ii*2+1].color.set(color);
+        oScene.add(cube);
+        setColor(cube, aData);
+
+        function setColor(oMesh, aData) {
+            let currFace = null;
+            let color = null;
+
+            let iX = aData[0][0].length;
+            let iY = aData[0].length;
+            let iZ = aData.length;
+            console.log("xyz ", iX, iY, iZ);
+            let dX, dY, dZ, sStatement; // eslint-disable-next-line
+            let aFace = new Array(6).fill(0).map(x => new Array());
+            let divide = (a, b) => ({
+                r: a % b,
+                d: Math.trunc(a / b)
+            });
+
+            //Find lowest and greatest temperature
+            let aFlat = aData.flat(2);
+            let iMin = Math.min.apply(null, aFlat);
+            let iMax = Math.max.apply(null, aFlat);
+
+            for (let ii = 0; ii < iX * iY * iZ; ii++) {
+                dX = divide(ii, iX);
+                dY = divide(dX.d, iY);
+                dZ = divide(dY.d, iZ);
+
+                sStatement = (dY.r === 0) * 1 + "" + (dY.r === iY - 1) * 1 + "" + (dX.r === 0) * 1 + "" + (dX.r === iX - 1) * 1 + "" + (dZ.r === 0) * 1 + "" + (dZ.r === iZ - 1) * 1;
+                for (let si = 0; si < sStatement.length; si++) {
+                    if (sStatement[si] === "1")(aFace[si]).push(aData[dZ.r][dY.r][dX.r]);
+                }
             }
-            face.geometry.elementsNeedUpdate = true;
+            console.log(aFace);
+
+
+            for (let ii = 0; ii < 6; ii++) {
+                currFace = oMesh.children[ii].geometry.faces;
+                let n = currFace.length / 2;
+                for (let ia = 0; ia < n; ia++) {
+                    color = tempToHSL(iMin, iMax, aFace[ii][ia]);
+                    currFace[ia * 2].color.set(color);
+                    currFace[ia * 2 + 1].color.set(color);
+                }
+                oMesh.children[ii].geometry.elementsNeedUpdate = true;
+            }
         }
 
-        function tempToHSL(min, max, temp){
+        function tempToHSL(min, max, temp) {
             let ret = 256 - (temp + min) * 256 / (max - min);
             ret = Math.floor(ret);
-            return "hsl("+ ret + ",100%,50%)";
+            return "hsl(" + ret + ",100%,50%)";
         }
 
-        function createAFace(faceSize, order, viewWireFrame = false){
-            let iWidth, iHeigh, vAngle, vPosition;
-
-            switch (order){
-                case 0:
-                    iWidth = faceSize.x;
-                    iHeigh = faceSize.z;
-                    vAngle = new THREE.Vector3(3*Math.PI/2,0,0);
-                    vPosition = new THREE.Vector3(
-                        iWidth* faceSize.tileSize/2,
-                        0,
-                        iHeigh* faceSize.tileSize/2
-                    );
-                    break;
-                case 1:
-                    iWidth = faceSize.x;
-                    iHeigh = faceSize.z;
-                    vAngle = new THREE.Vector3(Math.PI/2,0,0);
-                    vPosition = new THREE.Vector3(
-                        iWidth* faceSize.tileSize/2,
-                        faceSize.y * faceSize.tileSize,
-                        iHeigh* faceSize.tileSize/2,
-                    );
-                    break;
-                case 2:
-                    iWidth = faceSize.y;
-                    iHeigh = faceSize.z;
-                    vAngle = new THREE.Vector3(Math.PI/2,Math.PI/2,0);
-                    vPosition = new THREE.Vector3(
-                        0,
-                        iWidth* faceSize.tileSize/2,
-                        iHeigh* faceSize.tileSize/2
-                    );
-                    break;
-                case 3:
-                    iWidth = faceSize.y;
-                    iHeigh = faceSize.z;
-                    vAngle = new THREE.Vector3(3*Math.PI/2,Math.PI/2,0);
-                    vPosition = new THREE.Vector3(
-                        faceSize.x * faceSize.tileSize,
-                        iWidth* faceSize.tileSize/2,
-                        iHeigh* faceSize.tileSize/2
-                    );
-                    break;
-                case 4:
-                    iWidth = faceSize.x;
-                    iHeigh = faceSize.y;
-                    vAngle = new THREE.Vector3(0,0,0);
-                    vPosition = new THREE.Vector3(
-                        iWidth* faceSize.tileSize/2,
-                        iHeigh* faceSize.tileSize/2,
-                        0
-                    );
-                    break;
-                case 5:
-                    iWidth = faceSize.x;
-                    iHeigh = faceSize.y;
-                    vAngle = new THREE.Vector3(Math.PI,0,0);
-                    vPosition = new THREE.Vector3(
-                        iWidth* faceSize.tileSize/2,
-                        iHeigh* faceSize.tileSize/2,
-                        faceSize.z * faceSize.tileSize
-                    );
-                    break;
-                default:
-                    break;
+        function createAFace(faceSize, order, viewWireFrame = false) {
+            let oCurrFI;
+            let fPi = Math.PI;
+            let oFaceInfo = {
+                0: {
+                    width: 'x',
+                    height: 'z',
+                    angle: new THREE.Vector3(3 * fPi / 2, 0, 0),
+                    get position() {
+                        return new THREE.Vector3(faceSize[this.width] * faceSize.tileSize / 2, 0, faceSize[this.height] * faceSize.tileSize / 2)
+                    },
+                    side: THREE.BackSide
+                },
+                1: {
+                    width: 'x',
+                    height: 'z',
+                    angle: new THREE.Vector3(3 * fPi / 2, 0, 0),
+                    get position() {
+                        return new THREE.Vector3(faceSize[this.width] * faceSize.tileSize / 2, faceSize.y * faceSize.tileSize, faceSize[this.height] * faceSize.tileSize / 2)
+                    },
+                    side: THREE.FrontSide
+                },
+                2: {
+                    width: 'y',
+                    height: 'z',
+                    angle: new THREE.Vector3(3 * fPi / 2, 3 * fPi / 2, 0),
+                    get position() {
+                        return new THREE.Vector3(0, faceSize[this.width] * faceSize.tileSize / 2, faceSize[this.height] * faceSize.tileSize / 2)
+                    },
+                    side: THREE.FrontSide
+                },
+                3: {
+                    width: 'y',
+                    height: 'z',
+                    angle: new THREE.Vector3(3 * fPi / 2, 3 * fPi / 2, 0),
+                    get position() {
+                        return new THREE.Vector3(faceSize.x * faceSize.tileSize, faceSize[this.width] * faceSize.tileSize / 2, faceSize[this.height] * faceSize.tileSize / 2)
+                    },
+                    side: THREE.BackSide
+                },
+                4: {
+                    width: 'x',
+                    height: 'y',
+                    angle: new THREE.Vector3(fPi, 0, 0),
+                    get position() {
+                        return new THREE.Vector3(faceSize[this.width] * faceSize.tileSize / 2, faceSize[this.height] * faceSize.tileSize / 2, 0)
+                    },
+                    side: THREE.FrontSide
+                },
+                5: {
+                    width: 'x',
+                    height: 'y',
+                    angle: new THREE.Vector3(fPi, 0, 0),
+                    get position() {
+                        return new THREE.Vector3(faceSize[this.width] * faceSize.tileSize / 2, faceSize[this.height] * faceSize.tileSize / 2, faceSize.z * faceSize.tileSize)
+                    },
+                    side: THREE.BackSide
+                }
             }
+            oCurrFI = oFaceInfo[order];
+            console.log(order);
+            console.log(oCurrFI.position);
+            let tileGeometry = new THREE.PlaneGeometry(
+                faceSize[oCurrFI.width] * faceSize.tileSize,
+                faceSize[oCurrFI.height] * faceSize.tileSize,
+                faceSize[oCurrFI.width], faceSize[oCurrFI.height]
+            );
 
-            let tileGeometry = new THREE.PlaneGeometry( iWidth*faceSize.tileSize, iHeigh*faceSize.tileSize, iWidth, iHeigh );
-            let tileMaterial = new THREE.MeshBasicMaterial( {vertexColors: THREE.FaceColors, side: THREE.DoubleSide} );
-            let tileMesh = new THREE.Mesh( tileGeometry, tileMaterial );
-            tileMesh.rotation.setFromVector3(vAngle);
-            tileMesh.position.copy(vPosition);
+            let tileMaterial = new THREE.MeshBasicMaterial({
+                vertexColors: THREE.FaceColors,
+                side: oCurrFI.side
+            });
+            let tileMesh = new THREE.Mesh(tileGeometry, tileMaterial);
+            tileMesh.rotation.setFromVector3(oCurrFI.angle);
+            tileMesh.position.copy(oCurrFI.position);
 
-            if (viewWireFrame){
-                let wireFrameGeometry = new THREE.WireframeGeometry( tileGeometry );
-                let wireFrameMaterial = new THREE.LineBasicMaterial( { color: 0x0 } );
-                let wireFrame = new THREE.LineSegments( wireFrameGeometry, wireFrameMaterial );    
+            if (viewWireFrame) {
+                let wireFrameGeometry = new THREE.WireframeGeometry(tileGeometry);
+                let wireFrameMaterial = new THREE.LineBasicMaterial({
+                    color: 0x0
+                });
+                let wireFrame = new THREE.LineSegments(wireFrameGeometry, wireFrameMaterial);
                 tileMesh.add(wireFrame);
             }
             return tileMesh;
@@ -219,16 +292,16 @@ export default class SChart3DLite extends Component {
         this.aMesh[index].visible = true;
     };
 
-    handleHide(index){
+    handleHide(index) {
         let iSliceLevel = this.state.iSliceLevel;
         let iCrood = this.i2p(index)[this.state.sSliceAxis.toLowerCase()];
         let iLimit = this[this.state.sSliceAxis.toUpperCase()] - iSliceLevel;
-        
-        let innerBlock = function(oPos){
-            if (oPos.x>0&&oPos.y>0&&oPos.z>0) return true
+
+        let innerBlock = function (oPos) {
+            if (oPos.x > 0 && oPos.y > 0 && oPos.z > 0) return true
             return false;
         }
-        if ((iSliceLevel > 0 && iCrood >= iLimit)||innerBlock(this.i2p(index))) {
+        if ((iSliceLevel > 0 && iCrood >= iLimit) || innerBlock(this.i2p(index))) {
             this.aMesh[index].visible = false;
             return true;
         }
@@ -244,16 +317,15 @@ export default class SChart3DLite extends Component {
     }
 
     render() {
-        return ( <div><
-            div 
-            className = "chartContainer"
+        return ( < div > <
+            div className = "chartContainer"
             ref = {
                 thisDiv => {
                     this.container = thisDiv
                 }
             }
-            />
-            </div>
+            /> <
+            /div>
         );
     }
 }
