35e2482ace82e6b6bb3c060cd327ba132c52b96d
sillybread <quoctoan.c2b@gmail.com>
2020-11-07 22:08:49 +0700

    buid

---
+++ TOAN/3dchart/build/precache-manifest.2ebd5e5824e0cd92dc0e3c540025eca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6749dcc22e851d15034fd760715ce84b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538892145a45250089f9",
+    "url": "/static/css/2.583f6566.chunk.css"
+  },
+  {
+    "revision": "729d6e9da022f222ffd7",
+    "url": "/static/css/main.2ed91997.chunk.css"
+  },
+  {
+    "revision": "538892145a45250089f9",
+    "url": "/static/js/2.4d10ff4b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.4d10ff4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729d6e9da022f222ffd7",
+    "url": "/static/js/main.dd359540.chunk.js"
+  },
+  {
+    "revision": "67ecfe99ed7c8f44ef53",
+    "url": "/static/js/runtime-main.85e77acf.js"
+  }
+]);
