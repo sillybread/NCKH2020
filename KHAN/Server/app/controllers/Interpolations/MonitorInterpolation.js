59d118a94dabe242388b58af53d19dfe4ce0e45d
tranvikhan <tranvikhan@gmail.com>
2020-12-15 14:44:04 +0700

    fix

--- KHAN/Server/app/controllers/Interpolations/MonitorInterpolation.js
+++ KHAN/Server/app/controllers/Interpolations/MonitorInterpolation.js
@@ -2,16 +2,15 @@ exports.Get = (cubeData, areas) => {
   let coppy = [...areas];
   return coppy.map((area) => {
     let average =
-      Math.sum(
-        cubeData.values[area.size.x0][area.size.y0][area.size.z0],
-        cubeData.values[area.size.x0][area.size.y0][area.size.z1],
-        cubeData.values[area.size.x0][area.size.y1][area.size.z0],
-        cubeData.values[area.size.x0][area.size.y1][area.size.z1],
-        cubeData.values[area.size.x1][area.size.y0][area.size.z0],
-        cubeData.values[area.size.x1][area.size.y0][area.size.z1],
-        cubeData.values[area.size.x1][area.size.y1][area.size.z0],
-        cubeData.values[area.size.x1][area.size.y1][area.size.z1]
-      ) / 8;
+      (cubeData.values[area.size.x0][area.size.y0][area.size.z0] +
+        cubeData.values[area.size.x0][area.size.y0][area.size.z1] +
+        cubeData.values[area.size.x0][area.size.y1][area.size.z0] +
+        cubeData.values[area.size.x0][area.size.y1][area.size.z1] +
+        cubeData.values[area.size.x1][area.size.y0][area.size.z0] +
+        cubeData.values[area.size.x1][area.size.y0][area.size.z1] +
+        cubeData.values[area.size.x1][area.size.y1][area.size.z0] +
+        cubeData.values[area.size.x1][area.size.y1][area.size.z1]) /
+      8;
 
     return average != 90 ? { ...area, average: average, count: 8 } : null;
   });
